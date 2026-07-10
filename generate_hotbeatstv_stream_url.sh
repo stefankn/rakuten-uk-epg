9738a9fdcc68ad02e2c60e9dbcfdcfa55efab033
Stefan <1708541+stefankn@users.noreply.github.com>
2026-07-10 09:56:03 +0200

    Updated stream

--- generate_hotbeatstv_stream_url.sh
+++ generate_hotbeatstv_stream_url.sh
@@ -7,5 +7,6 @@ URL=$(streamlink --twitch-disable-ads --stream-url $CHANNEL_URL best)
 
 if [ -n "$URL" ]; then
 	echo "#EXTM3U" > "$OUTPUT_FILE"
+	echo "#EXT-X-STREAM-INF:BANDWIDTH=5000000" >> "$OUTPUT_FILE"
 	echo "$URL" >> "$OUTPUT_FILE"
 fi
