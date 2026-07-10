99dc39f82ad0aa06fa8facaf5c30c3b855c7d875
Stefan <1708541+stefankn@users.noreply.github.com>
2026-07-10 10:09:04 +0200

    Update script

--- generate_hotbeatstv_stream_url.sh
+++ generate_hotbeatstv_stream_url.sh
@@ -3,7 +3,7 @@
 CHANNEL_URL="https://www.twitch.tv/hotbeatstv"
 OUTPUT_FILE="./hotbeatstv.m3u8"
 
-URL=$(streamlink --twitch-disable-ads --stream-url $CHANNEL_URL best)
+URL=$(venv/bin/streamlink --twitch-disable-ads --stream-url $CHANNEL_URL best)
 
 if [ -n "$URL" ]; then
 	echo "#EXTM3U" > "$OUTPUT_FILE"
